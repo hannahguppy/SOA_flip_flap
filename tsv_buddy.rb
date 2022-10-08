885627a16aef80af4f21fc87eca388e33bebb0ca
jztroi <80251388+jztroi@users.noreply.github.com>
2022-10-08 20:11:46 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,28 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
+  NEWLINE = "\n"
+  TAB = "\t"
+
+  def row_to_table(headings, row)
+    row.map.with_index { |cell, i| [headings[i], cell] }
+  end
+
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    tsv_arr = tsv.split(NEWLINE).map { |line| line.split(TAB) }
+    headings = tsv_arr.first
+    rows = tsv_arr[1..]
+    @data = rows.map { |row| row_to_table(headings, row) }
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    headings = @data.first.to_h.keys.join(TAB)
+    rows = @data.map { |row| row.to_h.values.join(TAB) }
+    headings + NEWLINE + rows.join(NEWLINE) + NEWLINE
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,14 @@
+# frozen_string_literal: true
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  def take_yaml(yaml)
+    survey_arr = YAML.safe_load(yaml)
+    @data = survey_arr.map(&:to_a)
+  end
+
+  def to_yaml
+    @data.map(&:to_h).to_yaml
+  end
+end
